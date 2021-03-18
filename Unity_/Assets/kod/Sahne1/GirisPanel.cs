9afc49cc773e693e18cc56c73a56a7231a7dafbf
Canan Tokyay <tokyaycanan@gmail.com>
2021-03-18 11:41:23 +0300

    kullanici islemleri

---
+++ Unity_/Assets/kod/Sahne1/GirisPanel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GirisPanel : MonoBehaviour
+{
+    public TMP_InputField kullaniciAdi, sifre;
+    [Header("Deneme Kullanıcı")]
+    public string isim = "selim";
+    public string pass = "123";
+
+    PanelKontrol pK_Script;
+    SahneGecis sahneGecis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pK_Script = GetComponent<PanelKontrol>();
+        sahneGecis = GameObject.Find("SahneManager").GetComponent<SahneGecis>();
+    }
+
+    public void girisYap_B()
+    {
+        if (kullaniciAdi.text.Equals("") || sifre.text.Equals(""))
+        {
+            StartCoroutine(pK_Script.hataPanel("Boş BIRAKMAYINIZ!"));
+        }
+        else
+        {
+            //veritabanı
+
+
+            StartCoroutine(girisYap());
+        }
+    }
+
+    IEnumerator girisYap()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("unity", "girisYapma");
+        form.AddField("kullaniciAdi", kullaniciAdi.text); //selim
+        form.AddField("sifre", sifre.text);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/unity_DB/userRegister.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Sorgu Sonucu:" + www.downloadHandler.text);
+                if(www.downloadHandler.text.Contains("Giriş başarılı."))
+                {
+                    sahneGecis.sahneyiDegistir(1);
+                }else
+                    StartCoroutine(pK_Script.hataPanel(www.downloadHandler.text));
+            }
+        }
+    }
+}
---
+++ Unity_/Assets/kod/Sahne1/KayitOl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro; //TextMeshPro İçin
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;//UI objeleri için
+
+public class KayitOl : MonoBehaviour
+{
+    public TMP_InputField kullaniciAdi_k, sifre_k, sifret_k;
+    PanelKontrol pK_Script;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pK_Script = GetComponent<PanelKontrol>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void uyeligiOlustur_B()
+    {
+        if (kullaniciAdi_k.text.Equals("") || sifre_k.text.Equals("") || sifret_k.text.Equals(""))
+        {
+            StartCoroutine(pK_Script.hataPanel("Boş BIRAKMAYINIZ!"));
+
+        }
+        else
+        {
+
+            if (sifre_k.text.Equals(sifret_k.text))
+            {
+               Debug.Log("Veritabanı Bağlantısı");
+                    StartCoroutine(kayitOl());
+
+               
+            }
+            else
+            {
+                StartCoroutine(pK_Script.hataPanel("Şifreler Eşleşmiyor!"));
+            }
+        }
+    }
+
+
+    IEnumerator kayitOl()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("unity", "kayitOlma");
+        form.AddField("kullaniciAdi", kullaniciAdi_k.text); //selim
+        form.AddField("sifre", sifre_k.text);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/unity_DB/userRegister.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Sorgu Sonucu:" + www.downloadHandler.text);
+                StartCoroutine(pK_Script.hataPanel(www.downloadHandler.text));
+            }
+        }
+    }
+
+
+}
