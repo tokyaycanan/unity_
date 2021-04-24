b7703c26964cdea4f5e64ee7dafb7db3c1faccac
Hilal YILDIZ <65397194+yildizhilal@users.noreply.github.com>
2021-04-24 03:23:36 +0300

    nesne hareket

--- Unity_/Assets/kod/cams.cs
+++ Unity_/Assets/kod/cams.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class cams : MonoBehaviour
@@ -13,7 +14,11 @@ public class cams : MonoBehaviour
 
     public InputField gen;
     public InputField en;
-    // Start is called before the first frame update
+
+    public GameObject getTarget;
+    bool isMouseDragging;
+    Vector3 offsetValue;
+    Vector3 positionOfScreen;
     public void switchcam(int x)
     {
         float c;
@@ -31,12 +36,14 @@ public class cams : MonoBehaviour
         {
             cam1.transform.localPosition = new Vector3((a*10)/-2, 2.2f, b*(-3));
             cam1.enabled = true;
+            cam1 = cam1;
         }
         else if (x == 2)
         {
 
             cam2.transform.localPosition = new Vector3((a * 10) /-2, 2.2f, b*(6.4f));
             cam2.enabled = true;
+           cam1 = cam2;
         }
         else if (x == 3)
         {
@@ -44,16 +51,22 @@ public class cams : MonoBehaviour
 
             cam3.transform.localPosition = new Vector3((a * -10), 2.2f, 10);
             cam3.enabled = true;
+
+            cam1 = cam3;
         }
         else if (x == 4)
         {
 
             cam4.enabled = true;
+
+            cam1 = cam4;
         }
         else
         {
 
             cam5.enabled = true;
+
+            cam1 = cam5;
         }
 
     }
@@ -66,4 +79,56 @@ public class cams : MonoBehaviour
         cam4.enabled = false;
         cam5.enabled = false;
     }
+
+    void Update()
+    {
+
+        //Mouse Button Press Down
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            getTarget = ReturnClickedObject(out hitInfo);
+            if (getTarget != null)
+            {
+                isMouseDragging = true;
+                //Converting world position to screen position.
+                positionOfScreen = cam1.WorldToScreenPoint(getTarget.transform.position);
+                offsetValue = getTarget.transform.position - cam1.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, positionOfScreen.z));
+            }
+        }
+
+        //Mouse Button Up
+        if (Input.GetMouseButtonUp(0))
+        {
+            isMouseDragging = false;
+        }
+
+        //Is mouse Moving
+        if (isMouseDragging)
+        {
+            //tracking mouse position.
+            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, positionOfScreen.z);
+
+            //converting screen position to world position with offset changes.
+            Vector3 currentPosition = cam1.ScreenToWorldPoint(currentScreenSpace) + offsetValue;
+
+            //It will update target gameobject's current postion.
+            getTarget.transform.position = currentPosition;
+        }
+
+
+    }
+
+    //Method to Return Clicked Object
+    GameObject ReturnClickedObject(out RaycastHit hit)
+    {
+        GameObject target = null;
+        Ray ray = cam1.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            target = hit.collider.gameObject;
+        }
+        return target;
+    }
+
 }
